fd537eaf1761ffb9dbaabb8096245428c2e0730b
Andrew McDowell <madoliole@gmail.com>
2015-08-10 21:15:34 +1000

    CommonJS2 implementation -> module.exports = {funcName: funcName}

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var escodegen = require('escodegen');
 var ConstDependency = require("webpack/lib/dependencies/ConstDependency");
 var NullFactory = require("webpack/lib/NullFactory");
+var exports = [];
 
 function ClosureExposerPlugin(localization, functionName, failOnMissing) {
 
@@ -20,25 +21,55 @@ function isModuleExports(expr) {
 }
 
 
+function getExportsString() {
+  var exportsString = '';
+  for(var i=0; i<exports.length; i++) {
+    var ex = exports[i];
+    exportsString += ex.id.name + ': ' + ex.id.name;
+    if(i < exports.length - 1) {
+      exportsString += ', '
+    }
+  }
+  return exportsString;
+}
+
 ClosureExposerPlugin.prototype.apply = function(compiler) {
-	compiler.plugin("compilation", function(compilation, params) {
+  compiler.plugin("compilation", function(compilation, params) {
 		compilation.dependencyFactories.set(ConstDependency, new NullFactory());
 		compilation.dependencyTemplates.set(ConstDependency, new ConstDependency.Template());
 	});
 
 	compiler.parser.plugin("statement" , function(expr) {
-		console.log(isModuleExports(expr))
-		if(expr.type !== 'FunctionDeclaration') { return; }
+    /**
+     * COMMONJS2 - Implementation -> module.exports = {funcName: funcName}
+     */
+    if(isModuleExports(expr)) {
+      var exportsString = getExportsString();
+      var moduleExportsString = 'module.exports = {' + exportsString + '};';
+      var dep = new ConstDependency(moduleExportsString, expr.range);
+      dep.loc = expr.loc;
+      this.state.current.addDependency(dep);
+      return true;
+    }
 
-		var jsExpr = escodegen.generate(expr);
-		var prependString = 'exports.' + expr.id.name + ' = ';
-		var exportedExpression = prependString + jsExpr;
+		if(expr.type === 'FunctionDeclaration') {
 
-		var dep = new ConstDependency(exportedExpression, expr.range)
-		dep.loc = expr.loc;
-		this.state.current.addDependency(dep);
-		return true;
+    }
+    exports.push(expr);
+
+    /**
+     * PURE COMMON JS - Implementation -> exports.funcName = function funcName() {}
+     */
+		//var jsExpr = escodegen.generate(expr);
+		//var prependString = 'exports.' + expr.id.name + ' = ';
+		//var exportedExpression = prependString + jsExpr;
+    //
+		//var dep = new ConstDependency(exportedExpression, expr.range)
+		//dep.loc = expr.loc;
+		//this.state.current.addDependency(dep);
+		//return true;
 	});
+
 
 
 
